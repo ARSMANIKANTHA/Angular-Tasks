ffc5a73bfb6ad6db6127bcfecd89ca46df7c6a8b
ARSMANIKANTHA <s180273@rguktsklm.ac.in>
2023-09-06 19:12:15 +0530

    lts

---
+++ lts/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavigationComponent } from './navigation/navigation.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    NavigationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
