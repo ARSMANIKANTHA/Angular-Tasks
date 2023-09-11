3a6a5931f3da96d5e62a2c72af9a0c3e629ee7c1
ARSMANIKANTHA <s180273@rguktsklm.ac.in>
2023-09-11 11:11:38 +0530

    Changes

--- lts/src/app/registration/registration.component.ts
+++ lts/src/app/registration/registration.component.ts
@@ -9,6 +9,7 @@ export class RegistrationComponent implements OnInit {
 
   constructor() { }
   userData:any;
+  disable:any;
   ngOnInit(): void {
     this.userData = localStorage.getItem("users");
     if(this.userData)
@@ -29,14 +30,20 @@ export class RegistrationComponent implements OnInit {
   {
     console.log(val);
     const vals = this.users.filter(user=>user.email===val.value.email);
+    if(val.valid===true)
+    {
+      alert("Invalid Details Provided");
+    }
+    else{
     if(!vals.length)
     {
       this.users.push(val.value);
       localStorage.setItem("users",JSON.stringify(this.users)); 
+      // window.location.href='http://localhost:4200/login'
     }
     else{
       alert("User already exists");
-    }
+    }}
   }
 
 }
